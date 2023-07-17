e1bdb52a30a135ef90aab962ed181aebe92691ea
Caleb Rivera <calebrossr@gmail.com>
2023-07-16 20:28:18 -0400

    Remove token check

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,19 +1,10 @@
 import { Navbar } from "@/app/Navbar";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // TODO: Check if user is logged in
-  const hasToken = cookies().has("token") || true;
-
-  if (!hasToken) {
-    redirect("/login");
-  }
-
   return (
     <>
       <Navbar />
